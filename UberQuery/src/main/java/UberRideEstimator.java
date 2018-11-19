939f03bd7499c8e21270cada01db99dabca169e3
Ravi Munde <ravi72munde@gmail.com>
2018-11-19 02:09:31 -0500

    moved to system variables for api keys

--- UberQuery/src/main/java/UberRideEstimator.java
+++ UberQuery/src/main/java/UberRideEstimator.java
@@ -12,8 +12,8 @@ public class UberRideEstimator {
 
     private UberRideEstimator(){
         SessionConfiguration config = new SessionConfiguration.Builder()
-                .setClientId("F2NYyWRumnySXSinjEP7yb6xYsZdkWZ4")
-                .setServerToken("7JjU_CcpskC4IGukcMsYoLhg1iuUgNJGk-ZGLr6v")
+                .setClientId(System.getenv("uber_clientId"))
+                .setServerToken(System.getenv("uber_token"))
                 .build();
         ServerTokenSession session = new ServerTokenSession(config);
 
@@ -35,10 +35,10 @@ public class UberRideEstimator {
 
 
 
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
 //        UberRideEstimator estimatorService = UberRideEstimator.generateService();
 //        estimatorService.getPriceEstimates(37.7752315f,-122.418075f,37.7752415f,-122.518075f);
-//    }
+    }
 
 
 
