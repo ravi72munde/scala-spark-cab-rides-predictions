2b1ffb6a72b8c986a0974846bf4f76338e379af2
Ravi Munde <ravi72munde@gmail.com>
2018-11-24 04:15:22 -0500

    Flat maps and other optimizations

--- UberQuery/src/main/scala/Main.scala
+++ UberQuery/src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 
 import DynamoDB.{DynamoUberImpl, DynamoWeatherImp}
-import Models.{Location, LocationRepository, Weather}
+import Models.{CabPrice, Location, LocationRepository, Weather}
 import Rides.UberAPI
 import _root_.Weather.WeatherAPI
 import com.amazonaws.services.dynamodbv2.model.BatchWriteItemResult
@@ -13,27 +13,27 @@ import scala.concurrent.{Await, Future}
   * Work in progress
   */
 object Main extends App {
-  //  val source: Location =
-  //  val destination: Location = Location("cab-test2", 42.340064f, -71.089784f)
 
-  //  /*
-  //  Uber Price testing
-  //   */
-  //  val prices = UberAPI.getPrices(source, destination)
-  //  prices foreach println
-  //  DynamoUberImpl.put(prices)
-  //  /** **********************************/
-  //
-  //  /*
-  //  Weather Info testing
-  //   */
-  //  val getWeather: Location=>Option[Weather] = (x:Location) => WeatherAPI.getCurrentWeather(x)
-  //
-  //  val result: Seq[Weather] = ls.map(getWeather andThen(w=>w.get))
-  //  val futureResult: Future[Seq[BatchWriteItemResult]] = DynamoWeatherImp.put(result.toSet)
-  //  Await.result(futureResult,Duration.apply(10,"sec"))
-  //  futureResult.onComplete(println)
 
-  val repository = LocationRepository.getPairedLocations
-  repository foreach println
+  /*
+  Uber Price testing
+   */
+  val prices: Set[CabPrice] = {
+    LocationRepository.getPairedLocations.flatMap(l => UberAPI.getPrices(l._1, l._2)).toSet
+  }
+  val cabPriceResult: Future[Seq[BatchWriteItemResult]] = DynamoUberImpl.put(prices)
+
+  /** **********************************/
+
+  /*
+  Weather Info testing
+   */
+  val getWeather: Location => Option[Weather] = (x: Location) => WeatherAPI.getCurrentWeather(x)
+  val locations = LocationRepository.sourceSeq ++ LocationRepository.destinationSeq
+  val result: Seq[Weather] = locations.flatMap(l=>getWeather(l))
+  val futureResult: Future[Seq[BatchWriteItemResult]] = DynamoWeatherImp.put(result.toSet)
+
+  Await.result(futureResult, Duration.Inf)
+  Await.ready(cabPriceResult, Duration.Inf)
+
 }
