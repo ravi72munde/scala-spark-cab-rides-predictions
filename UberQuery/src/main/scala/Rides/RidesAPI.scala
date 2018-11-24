af2167c6b324575d389e63cc73c294bb06c2bf66
Ravi Munde <ravi72munde@gmail.com>
2018-11-24 04:15:22 -0500

    Flat maps and other optimizations

--- UberQuery/src/main/scala/Rides/RidesAPI.scala
+++ UberQuery/src/main/scala/Rides/RidesAPI.scala
@@ -30,13 +30,19 @@ object UberAPI extends RidesAPI {
     * @return set[SabPrices]
     */
   override def getPrices(source: Location, destination: Location): Set[CabPrice] = {
-    val ep: PriceEstimatesResponse = UberRideEstimator.getPriceEstimates(source, destination)
-    val priceSet: Set[PriceEstimate] = ep.getPrices.asScala.toSet
-    val prices: Set[CabPrice] = priceSet match {
-      case ps: Set[PriceEstimate] => ps.map(p => UberPriceModel(p, source, destination))
+
+
+    val priceSet: Option[Set[PriceEstimate]] = try {
+      val ep = UberRideEstimator.getPriceEstimates(source, destination)
+      Some(ep.getPrices.asScala.toSet)
+    } catch {
+      case e: Exception => None
+    }
+
+    priceSet match {
+      case ps: Some[Set[PriceEstimate]] => ps.get.map(p => UberPriceModel(p, source, destination))
       case _ => Set()
     }
-    prices
   }
 
 
