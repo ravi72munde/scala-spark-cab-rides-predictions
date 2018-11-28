3a337ec52592a9f165db53e1ec0ff40e8fa47b1c
Ravi Munde <ravi72munde@gmail.com>
2018-11-27 21:56:25 -0500

    Test class setup for scala

---
+++ UberQuery/src/test/scala/RidesSpec.scala
@@ -0,0 +1,18 @@
+import models.LocationRepository
+import org.mockito.MockitoSugar
+import org.mockito.MockitoSugar._
+import org.scalatest.{FlatSpec, Matchers}
+import rides.RidesAPI
+
+class RidesSpec extends FlatSpec with Matchers {
+
+  behavior of "UberAPI"
+
+  "An empty Set" should "have size 0" in {
+    val uberAPI = mock[RidesAPI]
+    MockitoSugar.doReturn(None).when(uberAPI).getPrices(LocationRepository.backBay,LocationRepository.fenway)
+    val result = uberAPI.getPrices(LocationRepository.backBay,LocationRepository.fenway)
+    println(result)
+  }
+
+}
